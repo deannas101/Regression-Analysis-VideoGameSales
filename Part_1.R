926325faee732ffec5885d45380687559c0e02fa
deannas101 <52871125+deannas101@users.noreply.github.com>
2020-10-07 18:09:15 -0400

    Update Part_1.R

--- Part_1.R
+++ Part_1.R
@@ -57,3 +57,37 @@ MSE <- SSE/(n-2)
 MSE
 
 ####Standard Errors B1 and B0####
+#need to find these equations
+
+####Confidence intervals b1 and b0####
+
+summary(regLine)
+b0 <- 78.018216
+b1 <- -0.053358
+tdist <- qt(0.975, n-2)
+sb0 <- sqrt(0.209837)
+sb1 <- sqrt(0.001017)
+
+b0.lower <- b0 - tdist*sb0
+b0.upper <- b0 + tdist*sb0
+
+b1.lower <- b1 - tdist*sb1
+b1.upper <- b1 + tdist*sb1
+
+#77.12002 < b0 < 78.91641
+#-0.115888 < b1 < 0.009171975
+
+####Confidence predictor X0####
+####Plotting confidence bands####
+
+sort.X <- sort(lifeExpectancy$Adult_Mortality)
+sort.fitted.Y <- regLine$fitted.values[order(lifeExpectancy$Adult_Mortality)]
+
+W2.val <- 2*qf(0.95,2,n-2)
+W.val <- sqrt(W2.val)
+
+Sxx <- sum((sort.X-mean(sort.X))^2)
+se.Y.hat <- sqrt(MSE * ((1/n) + (sort.X-mean(lifeExpectancy$Adult_Mortality))^2/Sxx))
+
+upper.band <- sort.fitted.Y + W.val*se.Y.hat
+lower.band <- sort.fitted.Y - W.val*se.Y.hat
