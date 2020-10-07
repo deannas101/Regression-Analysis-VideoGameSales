a02be4492d3e86d9151317104cd36dfafbcf8a11
deannas101 <52871125+deannas101@users.noreply.github.com>
2020-10-07 17:41:27 -0400

    Update Part_1.R

--- Part_1.R
+++ Part_1.R
@@ -1,19 +1,19 @@
 library(tidyverse)
 library(ggpubr)
 
-#Response variable: Life expectancy
+#Response variable: Life_expectancy
 #Numerical predictor variable: Adult_Mortality
 
-#removing missing data
+####removing missing data####
 lifeExpectancy <- Life_Expectancy_Data %>%
   select(c(Life_expectancy, Adult_Mortality)) %>%
   drop_na()
 
-#Scatter plot
+####Scatter plot####
 ggplot(lifeExpectancy, aes(x=Adult_Mortality, y=Life_expectancy)) +
   geom_point()
 
-#Histograms
+####Histograms####
 adultMortalityHist <- ggplot(lifeExpectancy, aes(x=Adult_Mortality)) +
   geom_histogram() +
   labs(title = "Adult Mortality", x= "Adult Deaths per 1000 people", y="")
@@ -24,10 +24,36 @@ lifeExpectancyHist <- ggplot(lifeExpectancy, aes(x=Life_expectancy)) +
 
 ggarrange(adultMortalityHist, lifeExpectancyHist)
 
-#Calculating variance of response
+####Calculating variance of response####
 responseVar <- round(var(lifeExpectancy$Life_expectancy),digits=3)
 responseVar
 
-#sample correlation
+####sample correlation####
 cor(lifeExpectancy$Life_expectancy, lifeExpectancy$Adult_Mortality)
 
+####point estimates of B1 and B0####
+regLine <- lm(Life_expectancy ~ Adult_Mortality, data = lifeExpectancy)
+summary(regLine)
+regLine$coefficients
+#b1 = -0.0533358
+#b0 = 78.018216
+
+####Fitted value calculation####
+regLine$fitted.values
+summary(regLine$fitted.values)
+
+####R2 and MSE####
+#R2 = SSR/SST
+anova(regLine)
+SSR <- 128741
+SSE <- 136750
+SST <- SSR + SSE
+R2 <- SSR/SST
+R2
+
+#MSE = SSE/(n-2)
+n <- nrow(lifeExpectancy)
+MSE <- SSE/(n-2)
+MSE
+
+####Standard Errors B1 and B0####
