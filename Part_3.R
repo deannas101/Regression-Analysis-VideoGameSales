2acdb04e2c14d870f3c9b0d77d2d8a9d9080faab
deannas101 <52871125+deannas101@users.noreply.github.com>
2020-12-10 14:57:01 -0500

    starting part 4

--- Part_3.R
+++ Part_3.R
@@ -3,71 +3,74 @@ library(lmtest)
 library(olsrr)
 library(tidyverse)
 
-#predictors: measles, alcohol, hepatitis b, adult mortality
+# predictors: measles, alcohol, hepatitis b, adult mortality
 
-#reduced data set with observations we're interested in
-red_data <- Life_Expectancy_Data %>% select(Life_expectancy, Adult_Mortality, Alcohol, Hepatitis_B, Measles) %>% drop_na()
+# reduced data set with observations we're interested in
+red_data <- Life_Expectancy_Data %>%
+  select(Life_expectancy, Adult_Mortality, Alcohol, Hepatitis_B, Measles) %>%
+  drop_na()
 
-####1####
-X <- as.matrix(red_data[,-1])
-X <- cbind(1,X)
-inv.XX <- solve(t(X)%*%X)
-H <- X%*%inv.XX%*%t(X)
+#### 1####
+X <- as.matrix(red_data[, -1])
+X <- cbind(1, X)
+inv.XX <- solve(t(X) %*% X)
+H <- X %*% inv.XX %*% t(X)
 lev <- diag(H)
 
-####2####
+#### 2####
 pairs(red_data)
 
-####3####
+#### 3####
 lmfit <- lm(Life_expectancy ~ ., data = red_data)
 res <- rstudent(lmfit)
 fit.Y <- fitted(lmfit)
 
-plot(res ~ fit.Y, xlab="Fitted values", ylab="Residual", main="Residual Plot against Fitted values")
-abline(h=0, lwd=2)
-plot(res ~ red_data$Adult_Mortality, xlab="X", ylab="Residual", main="Residual Plot against Adult Mortality")
-abline(h=0, lwd=2)
-plot(res ~ red_data$Alcohol, xlab="X", ylab="Residual", main="Residual Plot against Alcohol")
-abline(h=0, lwd=2)
-plot(res ~ red_data$Hepatitis_B, xlab="X", ylab="Residual", main="Residual Plot against Hepatitis B")
-abline(h=0, lwd=2)
-plot(res ~ red_data$Measles, xlab="X", ylab="Residual", main="Residual Plot against Measles")
-abline(h=0, lwd=2)
+plot(res ~ fit.Y, xlab = "Fitted values", ylab = "Residual", main = "Residual Plot against Fitted values")
+abline(h = 0, lwd = 2)
+plot(res ~ red_data$Adult_Mortality, xlab = "X", ylab = "Residual", main = "Residual Plot against Adult Mortality")
+abline(h = 0, lwd = 2)
+plot(res ~ red_data$Alcohol, xlab = "X", ylab = "Residual", main = "Residual Plot against Alcohol")
+abline(h = 0, lwd = 2)
+plot(res ~ red_data$Hepatitis_B, xlab = "X", ylab = "Residual", main = "Residual Plot against Hepatitis B")
+abline(h = 0, lwd = 2)
+plot(res ~ red_data$Measles, xlab = "X", ylab = "Residual", main = "Residual Plot against Measles")
+abline(h = 0, lwd = 2)
 
-####4####
-sqrt.lmfit <- lm(Life_expectancy ~ sqrt(X[,-1]), data=red_data)
+#### 4####
+sqrt.lmfit <- lm(Life_expectancy ~ sqrt(X[, -1]), data = red_data)
 res.sqrt.lmfit <- resid(sqrt.lmfit)
-qqnorm(res.sqrt.lmfit);qqline(res.sqrt.lmfit)
+qqnorm(res.sqrt.lmfit)
+qqline(res.sqrt.lmfit)
 
-####5####
+#### 5####
 p <- ncol(X)
 n <- nrow(X)
-which(lev > 2*p/n)
+which(lev > 2 * p / n)
 
 leveragePlots(lmfit)
 
-hist(lev, main="Histogram of leverage points", xlab="leverage")
-abline(v=2*p/n, lty=2, lwd=2, col="red")
+hist(lev, main = "Histogram of leverage points", xlab = "leverage")
+abline(v = 2 * p / n, lty = 2, lwd = 2, col = "red")
 
-####6####
+#### 6####
 ols_plot_cooksd_chart(lmfit)
 
-####7####
+#### 7####
 ols_plot_dffits(lmfit)
 
-####8####
+#### 8####
 ols_plot_dfbetas(lmfit)
 
-####9####
-#lack-of-fit: don't think this is necessary
+#### 9####
+# lack-of-fit: don't think this is necessary
 
-####10####
-#BP and BF test
+#### 10####
+# BP and BF test
 bptest(lmfit)
 
 lm.res <- resid(lmfit)
 
-#BF Test - Adult_Mortality
+# BF Test - Adult_Mortality
 mean.life <- mean(red_data$Adult_Mortality)
 
 sort.life <- sort(red_data$Adult_Mortality)
@@ -75,10 +78,10 @@ order.life <- order(red_data$Adult_Mortality)
 
 sort.res <- lm.res[order.life]
 
-group1.index <- which((sort.life < mean.life)|(sort.life ==mean.life))  
+group1.index <- which((sort.life < mean.life) | (sort.life == mean.life))
 group1 <- sort.life[group1.index]
 
-group2.index <- which((sort.life > mean.life))  
+group2.index <- which((sort.life > mean.life))
 group2 <- sort.life[group2.index]
 
 group1.res <- sort.res[group1.index]
@@ -87,9 +90,9 @@ group2.res <- sort.res[group2.index]
 d1 <- abs(group1.res - median(group1.res))
 d2 <- abs(group2.res - median(group2.res))
 
-t.test(d1,d2)
+t.test(d1, d2)
 
-#BF Test - Alcohol
+# BF Test - Alcohol
 mean.life <- mean(red_data$Alcohol)
 
 sort.life <- sort(red_data$Alcohol)
@@ -97,10 +100,10 @@ order.life <- order(red_data$Alcohol)
 
 sort.res <- lm.res[order.life]
 
-group1.index <- which((sort.life < mean.life)|(sort.life ==mean.life))  
+group1.index <- which((sort.life < mean.life) | (sort.life == mean.life))
 group1 <- sort.life[group1.index]
 
-group2.index <- which((sort.life > mean.life))  
+group2.index <- which((sort.life > mean.life))
 group2 <- sort.life[group2.index]
 
 group1.res <- sort.res[group1.index]
@@ -109,9 +112,9 @@ group2.res <- sort.res[group2.index]
 d1 <- abs(group1.res - median(group1.res))
 d2 <- abs(group2.res - median(group2.res))
 
-t.test(d1,d2)
+t.test(d1, d2)
 
-#BF Test - Hepatitis_B
+# BF Test - Hepatitis_B
 mean.life <- mean(red_data$Hepatitis_B)
 
 sort.life <- sort(red_data$Hepatitis_B)
@@ -119,10 +122,10 @@ order.life <- order(red_data$Hepatitis_B)
 
 sort.res <- lm.res[order.life]
 
-group1.index <- which((sort.life < mean.life)|(sort.life ==mean.life))  
+group1.index <- which((sort.life < mean.life) | (sort.life == mean.life))
 group1 <- sort.life[group1.index]
 
-group2.index <- which((sort.life > mean.life))  
+group2.index <- which((sort.life > mean.life))
 group2 <- sort.life[group2.index]
 
 group1.res <- sort.res[group1.index]
@@ -131,9 +134,9 @@ group2.res <- sort.res[group2.index]
 d1 <- abs(group1.res - median(group1.res))
 d2 <- abs(group2.res - median(group2.res))
 
-t.test(d1,d2)
+t.test(d1, d2)
 
-#BF Test - Measles
+# BF Test - Measles
 mean.life <- mean(red_data$Measles)
 
 sort.life <- sort(red_data$Measles)
@@ -141,10 +144,10 @@ order.life <- order(red_data$Measles)
 
 sort.res <- lm.res[order.life]
 
-group1.index <- which((sort.life < mean.life)|(sort.life ==mean.life))  
+group1.index <- which((sort.life < mean.life) | (sort.life == mean.life))
 group1 <- sort.life[group1.index]
 
-group2.index <- which((sort.life > mean.life))  
+group2.index <- which((sort.life > mean.life))
 group2 <- sort.life[group2.index]
 
 group1.res <- sort.res[group1.index]
@@ -153,7 +156,7 @@ group2.res <- sort.res[group2.index]
 d1 <- abs(group1.res - median(group1.res))
 d2 <- abs(group2.res - median(group2.res))
 
-t.test(d1,d2)
+t.test(d1, d2)
 
-####11####
+#### 11####
 shapiro.test(lm.res)
---
+++ Part_4.R
@@ -0,0 +1,40 @@
+
+
+# Re-creating data set with interaction terms ---------------------------------
+
+Life_Expectancy_Data <- as.data.frame(Life_Expectancy_Data)
+
+exp_data <- Life_Expectancy_Data[, 4:6]
+exp_data$V1 <- Life_Expectancy_Data$Hepatitis_B
+exp_data$V2 <- Life_Expectancy_Data$Measles
+exp_data$V3 <- Life_Expectancy_Data$BMI
+exp_data$V4 <- Life_Expectancy_Data$Diphtheria
+colnames(exp_data) <- c(
+  "Y", "X1", "X2", "X3", "X4", "X5", "X6"
+)
+
+X1X2 <- exp_data$X1 * exp_data$X2
+X1X3 <- exp_data$X1 * exp_data$X3
+X1X4 <- exp_data$X1 * exp_data$X4
+X1X5 <- exp_data$X1 * exp_data$X5
+X1X6 <- exp_data$X1 * exp_data$X6
+
+X2X3 <- exp_data$X2 * exp_data$X3
+X2X4 <- exp_data$X2 * exp_data$X4
+X2X5 <- exp_data$X2 * exp_data$X5
+X2X6 <- exp_data$X2 * exp_data$X6
+
+X3X4 <- exp_data$X3 * exp_data$X4
+X3X5 <- exp_data$X3 * exp_data$X5
+X3X6 <- exp_data$X3 * exp_data$X6
+
+X4X5 <- exp_data$X4 * exp_data$X5
+X4X6 <- exp_data$X4 * exp_data$X6
+
+X5X6 <- exp_data$X5 * exp_data$X6
+
+exp_data <- cbind(
+  exp_data, X1X2, X1X3, X1X4, X1X5, X1X6, X2X3, X2X4, X2X5, X2X6, X3X4,
+  X3X5, X3X6, X4X5, X4X6, X5X6
+)
+
